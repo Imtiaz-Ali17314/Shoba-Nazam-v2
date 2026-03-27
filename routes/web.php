f718fdae718d01df2fc4c9f56534b6c3c901b37b
Imtiaz Ali <imtiazali17314@gmail.com>
2026-03-27 12:02:49 +0500

    set routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// Home / root
 Route::get('/', function () {
     return view('app');
 });
+
+// Catch-all route for SPA (non-API)
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
